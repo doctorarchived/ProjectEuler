71dbd4e3764f515e164273f4df872ce0f9045b19
Michael <brady.m@outlook.com>
2016-01-25 18:46:46 -0500

    problem 104

---
+++ ProjectEuler/src/com/brady/euler/Problem104.java
@@ -0,0 +1,38 @@
+package com.brady.euler;
+
+public class Problem104 extends Problem {	
+	
+	@Override
+	public int solve() {
+		int fPrev = 1, f = 1;
+		for (int i = 3; i < 1000000; i++) {
+			int t = f;
+			f = (fPrev + f)  % 1000000000;
+			fPrev = t;
+			if (isPandigital(first9Digits(i)) && isPandigital(f)) {
+				return i;
+			}	
+		}
+		return -1;
+	}
+	
+	private int first9Digits(int n) {
+		double digits = n * 0.20898764024997873 - 0.3494850021680094;
+		return (int) Math.pow(10, digits - (int) digits + 9 - 1);
+	}
+	
+	private boolean isPandigital(int n) {
+        int digits = 0; int count = 0;
+
+        for (; n > 0; n /= 10, count++) {
+            if (digits == (digits |= 1 << (n - ((n / 10) * 10) - 1))) {
+                return false;
+            }
+        }
+
+        return digits == (1 << count) - 1;
+    }
+
+	
+	
+}
--- ProjectEuler/src/com/brady/euler/Solver.java
+++ ProjectEuler/src/com/brady/euler/Solver.java
@@ -10,7 +10,9 @@ public class Solver {
 	
 	public static void main(String [] args) {
 		//Problem.solve(Problem83.class,  new File("./p083_matrix.txt"));
-		Problem.solve(Problem75.class);
+		//Problem.solve(Problem75.class);
+		//Problem.solve(Problem96.class, new File("./p096_sudoku.txt"));
+		Problem.solve(Problem104.class);
 	}
 	
 	
